d3a250f4524e8ead31e539c8f8ac8ee8cc764291
jak3ym <jakemakaling@hotmail.com>
2025-06-09 18:39:15 +1000

    main: Updated About Me.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,6 +1,6 @@
-import { Code } from "lucide-react";
-import { User } from "lucide-react";
-import { Briefcase } from "lucide-react";
+import { Terminal } from "lucide-react";
+import { Cpu } from "lucide-react";
+import { Wifi } from "lucide-react";
 
 export const AboutSection = () => {
     return <section id="about" className="py-24 px-4 relative">
@@ -14,13 +14,8 @@ export const AboutSection = () => {
                     <h3 className="text-2xl font-semibold">Passionate Software/Hardware Engineer</h3>
 
                     <p className="text-muted-foreground">
-                        With over x years
+                        Passionate Embedded Software and Electronics Engineer in MedTech with 5+ years of product development experience, having expertise embedded software and hardware design, and integrating wireless technology.
                     </p>
-
-                    <p className="text-muted-foreground">
-                        I'm passionate.
-                    </p>
-
                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                         <a href="#contact" className="cosmic-button"> 
                             Get In Touch
@@ -36,12 +31,13 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary" />
+                                    <Terminal className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg"> Software Dev</h4>
+                                    <h4 className="font-semibold text-lg">Embedded Software</h4>
                                     <p className="text-muted-foreground">
-                                        Creating x and y from frameworks.
+                                        Developing software for embedded systems, focusing on C, C++, and Python script automation. Hands-on experience
+                                        deploying over AWS and managing CI/CD pipelines using Jenkins for streamlined software delivery.
                                     </p>
                                 </div>
                             </div>
@@ -49,12 +45,13 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <User className="h-6 w-6 text-primary" />
+                                    <Cpu className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg"> Hardware Eng</h4>
+                                    <h4 className="font-semibold text-lg">Hardware Design</h4>
                                     <p className="text-muted-foreground">
-                                        Designing bla bla bla.
+                                        Product Lead in designing and prototyping hardware systems (schematic capture and PCB layout using Altium Designer), 
+                                        and liaising with contract manufacturers from design to global mass production.
                                     </p>
                                 </div>
                             </div>
@@ -62,12 +59,12 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Briefcase className="h-6 w-6 text-primary" />
+                                    <Wifi className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg"> Project Management</h4>
+                                    <h4 className="font-semibold text-lg">Wireless Technology</h4>
                                     <p className="text-muted-foreground">
-                                        Leading projects
+                                        Integrating wireless technologies such as Bluetooth (BLE), Cellular (2G, 3G, 4G), and UWB into hardware, and directing wireless and EMC certifications.
                                     </p>
                                 </div>
                             </div>
